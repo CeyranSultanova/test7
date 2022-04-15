23e1481ce97eac957b0b8fe0087ada4c7ea45437
CeyranSultanova <ceyransultanova11@gmail.com>
2022-04-15 20:13:41 +0400

    test

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Ceyran", "Sultanova", 0);
+         
+          
+            Console.WriteLine(student1.FullInfo());
+        }      
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test7
+{
+    class Student
+    {
+        private string name;
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+        private string surname;
+        public string SurName {
+            get { return surname; }
+            set { surname = value; }
+        }
+        private int age;
+        public int Age {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+
+        public Student(string name ,string surname, int age)
+        {
+            this.Name = name;
+            this.SurName = surname;
+            this.Age = age;
+
+        }
+        //public int Mark
+        //{
+        //    get { return Mark; }
+        //    set
+        //    {
+        //        if (value > 0 && value < 100)
+        //        {
+        //            value = Mark;
+        //        }
+        //        else
+        //            Mark = 0;
+
+        //    }
+        //}
+        public string FullInfo()
+        {
+            return $" Name {name}, Surname {surname}, Age {age}";
+        }
+
+      
+        
+    }
+}
